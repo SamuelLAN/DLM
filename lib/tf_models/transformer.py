3720536a692a6be27b752271f81ef0930c1ad71b
alien of samuel <yusenlin@terpmail.umd.edu>
2020-03-23 18:39:04 -0400

    feat: add name scope for the functions in transformer

--- lib/tf_models/transformer.py
+++ lib/tf_models/transformer.py
@@ -11,32 +11,35 @@ def get_angles(pos, i, d_model):
 
 
 def positional_encoding(position, d_model):
-    angle_rads = get_angles(np.arange(position)[:, np.newaxis],
-                            np.arange(d_model)[np.newaxis, :],
-                            d_model)
+    with tf.name_scope('positional_encoding'):
+        angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                                np.arange(d_model)[np.newaxis, :],
+                                d_model)
 
-    # apply sin to even indices in the array; 2i
-    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+        # apply sin to even indices in the array; 2i
+        angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
 
-    # apply cos to odd indices in the array; 2i+1
-    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+        # apply cos to odd indices in the array; 2i+1
+        angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
 
-    pos_encoding = angle_rads[np.newaxis, ...]
+        pos_encoding = angle_rads[np.newaxis, ...]
 
-    return tf.cast(pos_encoding, dtype=tf.float32)
+        return tf.cast(pos_encoding, dtype=tf.float32)
 
 
 def create_padding_mask(seq):
-    seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
+    with tf.name_scope('padding_mask'):
+        seq = tf.cast(tf.math.equal(seq, 0), tf.float32)
 
-    # add extra dimensions to add the padding
-    # to the attention logits.
-    return seq[:, tf.newaxis, tf.newaxis, :]
+        # add extra dimensions to add the padding
+        # to the attention logits.
+        return seq[:, tf.newaxis, tf.newaxis, :]
 
 
 def create_look_ahead_mask(size):
-    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
-    return mask  # (seq_len, seq_len)
+    with tf.name_scope('look_ahead_mask'):
+        mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+        return mask  # (seq_len, seq_len)
 
 
 def scaled_dot_product_attention(q, k, v, mask):
@@ -57,24 +60,25 @@ def scaled_dot_product_attention(q, k, v, mask):
       output, attention_weights
     """
 
-    matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
+    with tf.name_scope('scaled_dot_product_attention'):
+        matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
 
-    # scale matmul_qk
-    dk = tf.cast(tf.shape(k)[-1], tf.float32)
-    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+        # scale matmul_qk
+        dk = tf.cast(tf.shape(k)[-1], tf.float32)
+        scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
 
-    # add the mask to th
-    # e scaled tensor.
-    if mask is not None:
-        scaled_attention_logits += (mask * -1e9)
+        # add the mask to th
+        # e scaled tensor.
+        if mask is not None:
+            scaled_attention_logits += (mask * -1e9)
 
-    # softmax is normalized on the last axis (seq_len_k) so that the scores
-    # add up to 1.
-    attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)  # (..., seq_len_q, seq_len_k)
+        # softmax is normalized on the last axis (seq_len_k) so that the scores
+        # add up to 1.
+        attention_weights = tf.nn.softmax(scaled_attention_logits, axis=-1)  # (..., seq_len_q, seq_len_k)
 
-    output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
+        output = tf.matmul(attention_weights, v)  # (..., seq_len_q, depth_v)
 
-    return output, attention_weights
+        return output, attention_weights
 
 
 class MultiHeadAttention(layers.Layer):
@@ -278,19 +282,20 @@ class Transformer(keras.Model):
 
     @staticmethod
     def create_masks(inp, tar):
-        # Encoder padding mask
-        enc_padding_mask = create_padding_mask(inp)
+        with tf.name_scope('mask'):
+            # Encoder padding mask
+            enc_padding_mask = create_padding_mask(inp)
 
-        # Used in the 2nd attention block in the decoder.
-        # This padding mask is used to mask the encoder outputs.
-        dec_padding_mask = create_padding_mask(inp)
+            # Used in the 2nd attention block in the decoder.
+            # This padding mask is used to mask the encoder outputs.
+            dec_padding_mask = create_padding_mask(inp)
 
-        # Used in the 1st attention block in the decoder.
-        # It is used to pad and mask future tokens in the input received by
-        # the decoder.
-        look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
-        dec_target_padding_mask = create_padding_mask(tar)
-        combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
+            # Used in the 1st attention block in the decoder.
+            # It is used to pad and mask future tokens in the input received by
+            # the decoder.
+            look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+            dec_target_padding_mask = create_padding_mask(tar)
+            combined_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
 
         return enc_padding_mask, combined_mask, dec_padding_mask
 
