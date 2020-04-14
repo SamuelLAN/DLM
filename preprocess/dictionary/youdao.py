9cfed3f3451441e6dcc6a968c2130b8650145a07
shuaicheng zhang <zshuai8@vt.edu>
2020-04-14 15:39:29 -0400

    adding internet explanations

--- preprocess/dictionary/youdao.py
+++ preprocess/dictionary/youdao.py
@@ -7,18 +7,19 @@ import json
 from concurrent.futures import ThreadPoolExecutor
 
 data_dict = {}
-def worker(input, english_indicator):
-    out = subprocess.Popen(['youdao', input],
+def worker(input):
+    quoted = "\"" + input + "\""
+    out = subprocess.Popen(['youdao', quoted],
        stdout=subprocess.PIPE,
        stderr=subprocess.STDOUT)
     check = out.communicate()[0].decode("utf-8").split("\n")
-    # print(check)
     for i in check[2:-2]:
         i = i.replace("\r", "")
         if "有道翻译" in i:
             data_dict[input] = {}
             data_dict[input]['translation'] = []
             data_dict[input]['translation'].append(i.replace("有道翻译：", ""))
+            continue
         # i = i.replace("[;,；，]", "")
         if "n." in i:
             if "pos" not in data_dict[input]:
@@ -28,6 +29,7 @@ def worker(input, english_indicator):
             for j in i.replace("n.", "").split("；"):
                 if j not in data_dict[input]["translation"] and j is not "":
                     data_dict[input]["translation"].append(j)
+            continue
         elif "v." in i:
             if "pos" not in data_dict[input]:
                 data_dict[input]['pos'] = []
@@ -36,6 +38,7 @@ def worker(input, english_indicator):
             for j in i.replace("v.", "").split("；"):
                 if j not in data_dict[input]["translation"] and j is not "":
                     data_dict[input]["translation"].append(j)
+            continue
         elif "adv." in i:
             if "pos" not in data_dict[input]:
                 data_dict[input]['pos'] = []
@@ -44,6 +47,7 @@ def worker(input, english_indicator):
             for j in i.replace("adv.", "").split("；"):
                 if j not in data_dict[input]["translation"] and j is not "":
                     data_dict[input]["translation"].append(j)
+            continue
         elif "adj." in i:
             if "pos" not in data_dict[input]:
                 data_dict[input]['pos'] = []
@@ -52,6 +56,7 @@ def worker(input, english_indicator):
             for j in i.replace("adj.", "").split("；"):
                 if j not in data_dict[input]["translation"] and j is not "":
                     data_dict[input]["translation"].append(j)
+            continue
         elif "prep." in i:
             if "pos" not in data_dict[input]:
                 data_dict[input]['pos'] = []
@@ -60,30 +65,34 @@ def worker(input, english_indicator):
             for j in i.replace("prep.", "").split("；"):
                 if j not in data_dict[input]["translation"] and j is not "":
                     data_dict[input]["translation"].append(j)
+            continue
+
         if english_indicator == "1":
-            tokens = i.split("[: ]")
-            if ":" in i and input.lower() == tokens[0].lower():
-                list_exp = tokens[1].split(" ")
-                for j in list_exp:
-                    if j not in data_dict[input]["translation"] and j is not "":
-                        data_dict[input]["translation"].append(j)
+            if ":" in i:
+                tokens = i.split("  :  ")
+                if input.lower() == tokens[0].lower():
+                    list_exp = tokens[1].split(" ")
+                    for j in list_exp:
+                        if j not in data_dict[input]["translation"] and j is not "":
+                            data_dict[input]["translation"].append(j)
 
 q = queue.Queue()
 num_wokers = 8
+english_indicator = sys.argv[2]
 with open(sys.argv[1]) as f:
-    check = sys.argv[2]
     data = json.load(f)
     filename = sys.argv[1].replace(".json", ".txt")
     filename = os.path.basename(filename)
-
+    print("start the threadpool for data")
     with ThreadPoolExecutor(max_workers=8) as executor:
         futures = []
         for i in list(data.keys()):
-            future = executor.submit(worker, (i, check))
+            future = executor.submit(worker, (i))
             futures.append(future)
         for i in futures:
             while not i.done():
                 continue
         filename = os.path.join(dictionary_dir, filename)
         with open(filename, 'w') as outfile:
-            json.dump(data, data_dict)
+            json.dump(data_dict, outfile)
+        print("Finished writing data to file", outfile)
