003e1ac072077beeba2f80a3057579783a247f40
alien of samuel <yusenlin@terpmail.umd.edu>
2020-04-28 20:07:48 -0400

    fix: adding stem dictionary

--- pretrain/preprocess/dictionary/stem.py
+++ pretrain/preprocess/dictionary/stem.py
@@ -12,7 +12,9 @@ dictionary = load_json(dictionary_path)
 
 print('getting stems ...')
 
-stem_words = list(map(lambda x: [utils.stem(x), x], dictionary.keys()))
+stem_words = list(dictionary.keys())
+stem_words = list(filter(lambda x: len(x.split(' ')) <= 1, stem_words))
+stem_words = list(map(lambda x: [utils.stem(x), x], stem_words))
 stem_words = list(filter(lambda x: x[0] != x[1], stem_words))
 
 print('converting to dict ...')
@@ -24,10 +26,27 @@ for i, (stem, word) in enumerate(stem_words):
         progress = float(i + 1) / len_stems * 100.
         print('\rprogress: %.2f%% ' % progress, end='')
 
-    stem_dictionary[stem] = word
+    if stem not in stem_dictionary:
+        stem_dictionary[stem] = []
+    stem_dictionary[stem].append(word)
+
+print('removing duplicate values ...')
+
+for stem, words in stem_dictionary.items():
+    words = list(set(words))
+    stem_dictionary[stem] = words
 
 print(f'\nwriting stem dictionary to file ...')
 
 write_json(stem_dictionary_path, stem_dictionary)
 
 print(f'\nlen_stem_dictionary: {len(stem_dictionary)}')
+
+"""
+The format of the stem dictionary is :
+    {
+        'like': ['likely', 'liking', 'alike'], 
+        'cry': ['cries', 'crying'],
+        ...
+    }
+"""
