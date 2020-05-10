404443c2ce668ec36af687861cba6adba7500580
shuaicheng zhang <zshuai8@vt.edu>
2020-05-09 23:01:58 -0400

    adding merge function

--- pretrain/preprocess/dictionary/merge.py
+++ pretrain/preprocess/dictionary/merge.py
@@ -1,3 +1,39 @@
+import json
+import os
+import sys
+from pretrain.preprocess.config import dictionary_dir
+
+def merge_dicts(dict_a, dict_b, union):
+    d = {}
+    dicts = [dict_a, dict_b]
+    for dict in dicts:
+        for key in list(dict):
+            for subKey in list(dict[key].keys()):
+                try:
+                    if union:
+                        d[key][subKey] = d[key][subKey] + list(set(dict[key][subKey]) - set(d[key][subKey]))
+                    else:
+                        continue
+                except KeyError:
+                    d[key] = (dict[key])
+    return d
+
+def mergeFiles(input, output):
+    data_lists = []
+    for  i in input:
+        with open(i ,encoding="utf-8") as f:
+            data = json.load(f)
+            data_lists.append(data)
+
+    out = {}
+    for i,k in zip(data_lists[0::2], data_lists[1::2]):
+        out = merge_dicts(out, i, k)
+    with open(os.path.join(output, "eng-keys.json"), 'w') as outfile:
+            json.dump(out, outfile)
 
 
+def main():
+    mergeFiles(sys.argv, dictionary_dir)
 
+if __name__ == "__main__":
+    main()
