ed0e78afe272f5bf893a0754dddb3b4b80ab9bf7
alien of samuel <yusenlin@terpmail.umd.edu>
2020-05-10 13:52:08 -0400

    ignore: reformat

--- pretrain/preprocess/dictionary/merge_file.py
+++ pretrain/preprocess/dictionary/merge_file.py
@@ -3,6 +3,7 @@ import os
 import sys
 from pretrain.preprocess.config import dictionary_dir
 
+
 def merge_dicts(dict_a, dict_b, union):
     d = {}
     dicts = [dict_a, dict_b]
@@ -18,22 +19,24 @@ def merge_dicts(dict_a, dict_b, union):
                     d[key] = (dict[key])
     return d
 
-def mergeFiles(input, output):
+
+def merge_files(_input, output):
     data_lists = []
-    for  i in input:
-        with open(i ,encoding="utf-8") as f:
+    for i in _input:
+        with open(i, encoding="utf-8") as f:
             data = json.load(f)
             data_lists.append(data)
 
     out = {}
-    for i,k in zip(data_lists[0::2], data_lists[1::2]):
+    for i, k in zip(data_lists[0::2], data_lists[1::2]):
         out = merge_dicts(out, i, k)
     with open(os.path.join(output, "eng-keys.json"), 'w') as outfile:
-            json.dump(out, outfile)
+        json.dump(out, outfile)
 
 
 def main():
-    mergeFiles(sys.argv, dictionary_dir)
+    merge_files(sys.argv, dictionary_dir)
+
 
 if __name__ == "__main__":
     main()
