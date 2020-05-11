37c3f7a7697f8d0cd2398468567cd3c340987401
alien of samuel <yusenlin@terpmail.umd.edu>
2020-05-10 20:00:43 -0400

    feat: adding word translation task loader

---
+++ pretrain/load/token_translation.py
@@ -0,0 +1,65 @@
+import random
+from pretrain.preprocess.config import filtered_union_en_zh_dict_path, filtered_union_zh_en_dict_path
+from lib.utils import load_json
+
+
+class Loader:
+    RANDOM_STATE = 42
+    NMT_TRAIN_RATIO = 0.98
+
+    def __init__(self, start_ratio=0.0, end_ratio=0.9, sample_ratio=1.0):
+        # load data from wmt_news
+        zh_en_dict = load_json(filtered_union_zh_en_dict_path)
+        en_zh_dict = load_json(filtered_union_en_zh_dict_path)
+
+        data = []
+        for zh, val in zh_en_dict.items():
+            if not val or 'translation' not in val or not val['translation']:
+                continue
+
+            for en in val['translation']:
+                data.append([zh, en])
+                data.append([en, zh])
+
+        for en, val in en_zh_dict.items():
+            if not val or 'translation' not in val or not val['translation']:
+                continue
+
+            for zh in val['translation']:
+                data.append([zh, en])
+                data.append([en, zh])
+
+        data = list(set(data))
+
+        # reproduce the process that nmt would go through in order to get its train set; shuffle the data
+        random.seed(self.RANDOM_STATE)
+        random.shuffle(data)
+
+        # sample data
+        data = self.sample_data(data, sample_ratio)
+
+        # split dataset
+        data = self.__split_data(data, start_ratio, end_ratio)
+
+        self.__src_data, self.__tar_data = list(zip(*data))
+
+    @staticmethod
+    def __split_data(data, start_ratio, end_ratio):
+        """ split data according to the ratio """
+        len_data = len(data)
+        start_index = int(len_data * start_ratio)
+        end_index = int(len_data * end_ratio)
+        return data[start_index: end_index]
+
+    @staticmethod
+    def sample_data(data, sample_rate):
+        len_data = len(data)
+        if sample_rate <= 1.0:
+            return data[: int(len_data * sample_rate)]
+        else:
+            return data * int(sample_rate) + data[: int(len_data * (sample_rate - int(sample_rate)))]
+
+    def data(self):
+        return self.__src_data, self.__tar_data
+
+
--- pretrain/preprocess/config.py
+++ pretrain/preprocess/config.py
@@ -15,3 +15,19 @@ filtered_zh_en_dict_path = os.path.join(dictionary_dir, 'filtered_zh_en_merged.j
 
 filtered_union_en_zh_dict_path = os.path.join(dictionary_dir, 'filtered_en_zh_merged_union.json')
 filtered_union_zh_en_dict_path = os.path.join(dictionary_dir, 'filtered_zh_en_merged_union.json')
+
+# vocabulary index
+start_nmt = 1
+end_nmt = 2
+mask = 3
+sep = 4
+start_cdlm_t = 5
+end_cdlm_t = 6
+start_cdlm_pos = 7
+end_cdlm_pos = 8
+start_cdlm_ner = 9
+end_cdlm_ner = 10
+start_cdlm_synonym = 11
+end_cdlm_synonym = 12
+start_cdlm_def = 13
+end_cdlm_def = 14
