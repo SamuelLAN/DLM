561e858651ba5082b8195df51016ee11e7fac504
alien of samuel <yusenlin@terpmail.umd.edu>
2020-05-14 11:39:02 -0400

    fix: fix out of mem bug

--- lib/metrics/metrics.py
+++ lib/metrics/metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import platform
 
 
 def accuracy(y_true, y_pred):
@@ -7,14 +8,26 @@ def accuracy(y_true, y_pred):
     return np.mean(y_pred == y_true)
 
 
+def __one_hot(y_true, depth):
+    _shape = list(y_true.shape)
+    z = np.zeros(_shape + [depth], dtype=np.int32)
+
+    for i in range(_shape[0]):
+        for j in range(_shape[1]):
+            z[i, j, int(y_true[i, j])] = 1
+
+    return z
+
+
 def perplexity(y_true, y_pred):
     y_true = np.reshape(y_true, [-1, y_pred.shape[1]])
     # mask = np.expand_dims(np.cast[np.float32](np.logical_not(np.equal(y_true, 0))), axis=-1)
-    try:
+
+    if 'windows' in platform.platform().lower():
         y_true = np.cast[np.float32](np.eye(y_pred.shape[-1])[np.cast[np.int32](y_true)])
-    except:
-        print('memory exceed')
-        return -1
+    else:
+        y_true = __one_hot(y_true, y_pred.shape[-1])
+
     # loss = - (y_true * np.log(y_pred + 0.0001) + (1 - y_true) * np.log(1 - y_pred + 0.0001))
     loss = - y_true * np.log(y_pred + 0.0001)
 
