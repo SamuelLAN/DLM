166fcacec202b099a8c67d0c5d02890bd39d15bb
alien of samuel <yusenlin@terpmail.umd.edu>
2020-05-16 07:53:14 -0400

    feat: change the perplexity function

--- lib/metrics/metrics.py
+++ lib/metrics/metrics.py
@@ -20,20 +20,20 @@ def __one_hot(y_true, depth):
 
 
 def perplexity(y_true, y_pred):
-    y_true = np.reshape(y_true, [-1, y_pred.shape[1]])
+    # y_true = np.reshape(y_true, [-1, y_pred.shape[1]])
     # mask = np.expand_dims(np.cast[np.float32](np.logical_not(np.equal(y_true, 0))), axis=-1)
 
-    if 'windows' in platform.platform().lower():
-        y_true = np.cast[np.float32](np.eye(y_pred.shape[-1])[np.cast[np.int32](y_true)])
-    else:
-        y_true = __one_hot(y_true, y_pred.shape[-1])
+    # if 'windows' in platform.platform().lower():
+    #     y_true = np.cast[np.float32](np.eye(y_pred.shape[-1])[np.cast[np.int32](y_true)])
+    # else:
+    #     y_true = __one_hot(y_true, y_pred.shape[-1])
 
-    # loss = - (y_true * np.log(y_pred + 0.0001) + (1 - y_true) * np.log(1 - y_pred + 0.0001))
-    loss = - y_true * np.log(y_pred + 0.0001)
+    # loss = - y_true * np.log(y_pred + 0.0001)
+    loss = - y_pred * np.log(y_pred + 0.0001)
 
     # loss *= mask
     loss = np.sum(loss, axis=-1)
     loss = np.mean(loss)
 
-    _perplexity = np.exp(loss)
+    _perplexity = np.exp2(loss)
     return _perplexity
--- lib/tf_metrics/pretrain.py
+++ lib/tf_metrics/pretrain.py
@@ -10,6 +10,18 @@ def tf_accuracy(y_true, y_pred):
 
 def tf_perplexity(y_true, y_pred):
     with tf.name_scope('perplexity'):
-        y_true = tf.reshape(y_true, [-1, y_pred.shape[1]])
-        y_true = tf.cast(tf.one_hot(tf.cast(y_true, tf.int32), y_pred.shape[-1]), y_pred.dtype)
-        return tf.exp(tf.losses.categorical_crossentropy(y_true, y_pred))
+        # y_true = tf.reshape(y_true, [-1, y_pred.shape[1]])
+        # y_true = tf.cast(tf.one_hot(tf.cast(y_true, tf.int32), y_pred.shape[-1]), y_pred.dtype)
+        # return tf.exp(tf.losses.categorical_crossentropy(y_true, y_pred))
+
+        # y_true = tf.reshape(y_true, [-1, y_pred.shape[1]])
+        # mask = tf.expand_dims(tf.cast(tf.logical_not(tf.equal(y_true, 0)), y_pred.dtype), axis=-1)
+
+        loss = - y_pred * tf.math.log(y_pred + 0.0001)
+
+        # loss *= mask
+        loss = tf.reduce_sum(loss, axis=-1)
+        loss = tf.reduce_mean(loss)
+
+        _perplexity = tf.pow(2, loss)
+        return _perplexity
