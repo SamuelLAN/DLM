1e60267ce84cebe4da4a35b810229f66004e3b11
Samuel_Alien <linyusensamuel@gmail.com>
2020-06-25 21:35:11 -0400

    feat: add more log for pretrain models

--- nmt/models/base_model.py
+++ nmt/models/base_model.py
@@ -13,7 +13,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from matplotlib.pylab import mpl
 
-mpl.rcParams['font.sans-serif'] = ['FangSong']  # 指定默认字体
+mpl.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
 mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
 
 keras = tf.keras
